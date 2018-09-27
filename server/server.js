26a61fc4dbfbbbe8d7652fd2b229c7986e9100ff
Greg Orvik <gvorvik@gmail.com>
2018-09-27 12:30:00 -0500

    authenticate, login, logout, and sign up the users

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://gregory16:gregory16@ds263380.mlab.com:63380/auth';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
