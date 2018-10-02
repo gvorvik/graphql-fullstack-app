9c1b10a7356456826de06720fa58bb672e8916c0
Greg Orvik <gvorvik@gmail.com>
2018-10-02 12:32:42 -0500

    login and logout user and update header

---
+++ client/components/LoginForm.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import AuthForm from './AuthForm';
+import {graphql} from 'react-apollo';
+import mutation from '../mutations/Login';
+import query from '../queries/currentUser';
+
+class LoginForm extends Component {
+
+    onSubmit({email, password}) {
+        this.props.mutate({
+            variables: {
+                email,
+                password
+            },
+            refetchQueries: [{query}]
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <h3>Login</h3>
+                <AuthForm 
+                    onSubmit={this.onSubmit.bind(this)}
+                />
+            </div>
+        );
+    }
+}
+
+export default graphql(mutation)(LoginForm);
