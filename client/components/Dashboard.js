d22f8ad2eeb242866dca858d0239f5aaadbb9ef3
Greg Orvik <gvorvik@gmail.com>
2018-10-02 13:52:17 -0500

    set up signup form and mutation

---
+++ client/components/Dashboard.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Dashboard extends Component {
+    render() {
+        return (
+            <div>Welcome to the app!</div>
+        );
+    }
+}
+
+export default Dashboard;
--- client/components/SignupForm.js
+++ client/components/SignupForm.js
@@ -1,11 +1,31 @@
 import React, {Component} from 'react';
+import AuthForm from './AuthForm';
+import {graphql} from 'react-apollo';
+import mutation from '../mutations/Signup';
 
 class SignupForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            errors: []
+        }
+    }
+
+    onSubmit({email, password}) {
+        this.props.mutate({
+            variables: {email, password}
+        });
+    }
+
     render() {
         return (
-            <div>Signup Form</div>
+            <div>
+                <h3>Signup</h3>
+                <AuthForm errors={this.state.errors} onSubmit={this.onSubmit.bind(this)}/>
+            </div>
         );
     }
 }
 
-export default SignupForm;
+export default graphql(mutation)(SignupForm);
