21c6b08be4b04ec101e68f10f4cf4fcc5efb75f6
Greg Orvik <gvorvik@gmail.com>
2018-10-03 09:41:26 -0500

    reroute to login on logout button click

---
+++ client/components/requireAuth.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import {graphql} from 'react-apollo';
+import currentUserQuery from '../queries/currentUser';
+import {hashHistory} from 'react-router'
+
+
+export default (WrappedComponent) => {
+
+    class RequireAuth extends Component {
+        componentWillUpdate(nextProps) {
+            if(!nextProps.data.currentUser && !nextProps.data.loading) {
+              hashHistory.push('/login');
+            }
+        }
+
+        render() {
+            return <WrappedComponent {...this.props}/>
+        }
+    }
+
+
+
+    return graphql(currentUserQuery)(RequireAuth);
+}
